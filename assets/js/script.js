5ea8944fe58765b0a364399a0769acec9364ee45
James Capadocia <james.capadocia@gmail.com>
2021-08-16 13:46:59 -0500

    Created function 'prepareQuiz' to set stage for quiz to begin -- showing/hiding proper buttons when quiz starts as well as changing introduction text to first question in quiz. Also began working on answer buttons to work as intended and to implement a scoring component in the project.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
+document.getElementById("lets-begin").addEventListener("click", countdown);
 var timeRemain = document.getElementById('timer');
 
 function countdown() {
-  var timeLeft = 30;
+
+  var timeLeft = 10;
 
   var timeInterval = setInterval(function () {
     if (timeLeft > 1) {
@@ -19,3 +21,41 @@ function countdown() {
   }, 1000);
 }
 
+document.getElementById("lets-begin").addEventListener("click", prepareQuiz);
+
+var question = document.getElementById("big-words")
+var hideSmall = document.getElementById("small-words")
+var hideStartQuiz = document.getElementById("lets-begin");
+var showAnswers = document.getElementById("answer1");
+
+function prepareQuiz() {
+  question.textContent = "Question 1";
+  hideSmall.style.display = "none";
+  hideStartQuiz.style.display = "none";
+  showAnswers.style.display = "block";
+}
+
+var answer1 = document.getElementById("answer1").addEventListener("click", quizTime);
+// var answer2 = document.getElementById("answer2").addEventListener("click");
+// var answer3 = document.getElementById("answer3").addEventListener("click");
+// var answer4 = document.getElementById("answer4").addEventListener("click");
+
+function quizTime() {
+
+  var score = 0
+
+  if (answer1) {
+    console.log("it works!");
+    score++;
+    console.log(score)
+ }
+//  if (entry) {
+//     console.log("entry");
+//  } 
+//  if (entry) {
+//     console.log("entry");
+//  } 
+//  if (entry) {
+//     console.log("entry");
+//  } 
+}
